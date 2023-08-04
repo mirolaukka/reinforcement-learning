fb16a34222b6357d686ea784093b1227a6b3dbdb
Miro Laukka <miro.laukka@kotimail.net>
2023-08-04 14:59:34 +0300

    fixed visualization

--- cart-pole/main.py
+++ cart-pole/main.py
@@ -4,25 +4,26 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
 # Load environment
-visualize = False
+visualize = True
 environment_name = "CartPole-v0"
-if visualize:
-    env = gym.make(environment_name, render_mode="human")
-else:
-    env = gym.make(environment_name)
+env = gym.make(environment_name)
 
 # Train an RL Model
 env = DummyVecEnv([lambda: env])
 model = PPO('MlpPolicy', env, verbose=1)
 model.learn(total_timesteps=9_000)
 
+
+if visualize:
+    env = gym.make(environment_name, render_mode="human")
+
+
 # Evaluate the Model
-evaluated = evaluate_policy(model, env, n_eval_episodes=15,
+evaluated = evaluate_policy(model, env, n_eval_episodes=2,
                             render=True, return_episode_rewards=True, warn=False)
-print(evaluated)
 
 # Print the evaluation results
 for i, e in enumerate(evaluated[0]):
-    print(i, e)
+    print("Episode: ", i,  "Rewards: ", e)
 print("Mean:", round(sum(evaluated[0])/len(evaluated[0]), 2))
 env.close()
